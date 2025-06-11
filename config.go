0d11d6bb395276635dc63affb85ba5f4be7fff4d
Alexander Goussas <goussasalexander@gmail.com>
2025-06-10 22:36:10 -0500

    feat: use the dedupe library to deduplicate span attributes

--- config.go
+++ config.go
@@ -1,9 +1,35 @@
 package dedupeprocessor
 
-import "go.opentelemetry.io/collector/component"
+import (
+	"errors"
 
-type Config struct{}
+	"go.opentelemetry.io/collector/component"
+	"slices"
+)
+
+type TableType string
+
+type Config struct {
+	tableType TableType `mapstructure:"table-type"`
+}
+
+const (
+	REDIS_TABLE TableType = "redis"
+)
+
+var (
+	validTables = []TableType{REDIS_TABLE}
+)
 
 func createDefaultConfig() component.Config {
-	return &Config{}
+	return &Config{
+		tableType: REDIS_TABLE,
+	}
+}
+
+func (c *Config) Validate() error {
+	if slices.Contains(validTables, c.tableType) {
+		return nil
+	}
+	return errors.New("invalid table type: " + string(c.tableType))
 }
--- factory.go
+++ factory.go
@@ -2,7 +2,9 @@ package dedupeprocessor
 
 import (
 	"context"
+	"errors"
 
+	"github.com/Tracing-Performance-Labs/go-dedupe"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/processor"
@@ -21,7 +23,19 @@ func createTracesProcessor(
 	cfg component.Config,
 	nextConsumer consumer.Traces,
 ) (processor.Traces, error) {
+	oCfg := cfg.(*Config)
+
+	var codec *dedupe.Codec
+
+	switch oCfg.tableType {
+	case REDIS_TABLE:
+		codec = dedupe.NewCodec(dedupe.WithRedisTable())
+	default:
+		return nil, errors.New("invalid table type: " + string(oCfg.tableType))
+	}
+
 	return newTracesProcessor(
+		codec,
 		ctx,
 		set,
 		cfg.(*Config),
